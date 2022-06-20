1eff1ff41eb21bb2bc0d15124216e6521283d6d8
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-20 08:56:19 -0300

    aaaaa

---
+++ app.js
@@ -0,0 +1,13 @@
+ function olamundo() {
+     console.log("olamundo");
+ }
+olamundo();
+function nome(leno) {
+    console.log(`Olá ${leno}`);
+}
+nome("leno")
+
+function estilos(leno, vintedois, rock) {
+    console.log(`Sou ${leno}, tenho ${vintedois}e gosto de ${rock}`);
+}
+​estilos("leno", "vintedois", "rock");
---
+++ olamundo.js
@@ -0,0 +1,3 @@
+function olamundo() {
+  console.log("Ola, mundo");
+}
