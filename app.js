996c34c53259730c968a7fe6f4c31ae18c3c5e78
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-20 14:12:53 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-//  function olamundo() {
-//      console.log("olamundo");
-//  }
-// olamundo();
-// function nome(leno) {
-//     console.log(`Olá ${leno}`);
-// }
-// nome("leno")
-
-
-// function Sobre(mechamo, idade, musica){
-//     console.log(Meu nome é ${mechamo}, tenho ${idade},e gosto de ${musica})
-// }
-function estilo(nome, idade, estiloMusical){
-    console.log(Sou ${nome}, tenho ${idade},e gosto de ${estiloMusical})
+ function olamundo() {
+     console.log("olamundo");
+ }
+olamundo();
+function nome(leno) {
+    console.log(`Olá ${leno}`);
 }
+nome("leno")
 
-estilo('leno', 22 ,'rock')
+
+
+function estilo(nome, idade, estiloMusical){
+    console.log(`Sou ${nome}, tenho ${idade},e gosto de ${estiloMusical}`)
+}
+estilo('Leno', 22, 'Rock')
+
+
+function diversa( filme, musica){
+    console.log(`Meu filme preferido é ${filme}, e musica é ${musica}`)
+}
+diversa('deadpool','luiz gonzaga')
+
+
+function triplo(x){
+  return console.log(x*3)
+}
+triplo(14)
